58f8ea60b5435bafed84c304350fe73b328fe293
hatice seremet <haticeesrmt52@gmail.com>
2024-03-18 16:22:07 -0400

    Kahn system

--- kpn.cpp
+++ kpn.cpp
@@ -3,4 +3,35 @@
 
 #include "kpn.h"
 
-// ADD THINGS HERE
+void kpn::add_process() {
+    while (true) {
+        unsigned int a = fifo_a.read();
+        unsigned int c = fifo_c.read();
+        unsigned int b = a + c;
+        fifo_b.write(b);
+    }
+}
+
+void kpn::split_process() {
+    int print_count = 0;
+    while (print_count < 10) {
+        unsigned int b = fifo_b.read();
+        fifo_a.write(b);
+        fifo_d.write(b);
+        out_e.write(b);
+        std::cout << "Split Output e: " << b << std::endl;
+        print_count++;
+        if (print_count >= 10) {
+            sc_stop(); // Stop the simulation
+        }
+    }
+}
+
+void kpn::delay_process() {
+    while (true) {
+        unsigned int d = fifo_d.read();
+        fifo_c.write(d);
+    }
+}
+
+// sc_main remains in a separate file (main.cpp) as usual.
--- kpn.h
+++ kpn.h
@@ -7,11 +7,31 @@ SC_MODULE(kpn)
 {
 private:
     // ADD THINGS HERE
+    sc_fifo<unsigned int> fifo_a, fifo_b, fifo_c, fifo_d;
+    sc_signal<unsigned int> internal_e_signal;
+    // Output port for the Split process to output signal e
 
+    // Internal SC_THREAD processes
+    void add_process();
+    void split_process();
+    void delay_process();
 public:
-    SC_CTOR(kpn) // : ADD THINGS HERE
+    sc_out<unsigned int> out_e;
+
+
+    SC_CTOR(kpn):fifo_a(10), fifo_b(10), fifo_c(10), fifo_d(10), out_e("out_e") // : ADD THINGS HERE
     {
         // ADD THINGS HERE
+                // Initialize the FIFOs with starting values
+        fifo_b.write(1);
+        fifo_c.write(0);
+
+        // Register the SC_THREAD processes
+        SC_THREAD(add_process);
+        SC_THREAD(split_process);
+        SC_THREAD(delay_process);
+
+        out_e(internal_e_signal);
     }
 };
 
